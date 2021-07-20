1e70189c54462f80f0cb732eff9a6e2051f44d47
Manuel <auelrs@hotmail.com>
2021-07-20 11:17:29 +0200

    Proyecto creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsultaComponent } from './empleados/consulta/consulta.component';
+import { ListadoComponent } from './empleados/listado/listado.component';
+import { ToledoComponent } from './ciudades/toledo/toledo.component';
+import { MadridComponent } from './ciudades/madrid/madrid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultaComponent,
+    ListadoComponent,
+    ToledoComponent,
+    MadridComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ciudades/toledo/toledo.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ToledoComponent } from './toledo.component';
+
+describe('ToledoComponent', () => {
+  let component: ToledoComponent;
+  let fixture: ComponentFixture<ToledoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ToledoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ToledoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
